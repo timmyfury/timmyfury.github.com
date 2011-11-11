c172fd2c7a1426cfc405975daceb84b7a352b3b1
Tim Crowe <tim.crowe@timmyfury.com>
2011-11-11 16:00:02 -0500

    starting filtering behavior

--- resume/behavior.js
+++ resume/behavior.js
@@ -1,3 +1,77 @@
 (function($){
 
+	/*
+		jQuery
+	**********************************************************************/
+	$.extend({
+
+		distinct: function(array) {
+			var distinct = [];
+
+			$.each(array, function(i, v){
+				if($.inArray(v, distinct) == -1){
+					distinct.push(v);
+				};
+			});
+
+			return distinct.sort();
+		}
+
+	});
+
+	$.fn.extend({
+
+		getClasses: function(){
+			var classes = [];
+			this.each(function(i, e){
+				var classStr = $(e).attr('class');
+				if(classStr != '' && classStr != null && typeof classStr != 'undefined'){
+					var classList = classStr.split(/\s+/);
+					for(var c = 0; c < classList.length; c++){
+						classes.push(classList[c]);
+					}
+				}
+			});
+			return $.distinct(classes);
+		}
+
+	});
+
+	/*
+		Filter
+	**********************************************************************/
+	function Filter(opts){
+
+		this.opts = $.extend({
+			
+		}, opts);
+
+		return this;
+
+	}
+
+	(function(filterproto){
+
+		filterproto.toString = function(){ return "[object Filter]"; };
+
+	}(Filter.prototype));
+
+	window.Filter = Filter;
+
+	/*
+		Filter
+	**********************************************************************/
+	$(function(){
+
+		var skills = $("#skills dt").getClasses();
+
+		for(var i = 0; i < skills.length; i++){
+			var skill = skills[i],
+				filters = $('dt.' + skill + ', ' + 'dd.' + skill),
+				label = $('dt.' + skill).text();
+
+			console.log(label, ' - ', skill, filters);
+		}
+
+	});
 }(jQuery));
